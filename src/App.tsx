621d8c1ee35044a99132842c727729d0cd94403b
Sweta Shaw <swt.shw94@gmail.com>
2023-09-07 13:28:54 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ImportReclaimProofs } from '@swetshaw/react-component'
+import { ChakraProvider, Flex } from '@chakra-ui/react'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Flex alignItems='center' justifyContent='center' w='100%' h='100vh'>
+      <ImportReclaimProofs label='Import from Reclaim' webHook='jhvh' isProofsReceived={true} />
+      </Flex>
+    </ChakraProvider>
   );
 }
 
